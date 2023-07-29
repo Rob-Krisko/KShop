f2587ec1c2999284f72c4056c8604f9ccbd519ec
Rob-Krisko <kriskora22@gmail.com>
2023-07-29 15:30:05 -0400

    cart updates

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -3,39 +3,51 @@ import { CartContext } from '../context/CartContext';
 import styled from 'styled-components';
 import { FiPlusCircle, FiMinusCircle, FiXCircle } from 'react-icons/fi';
 
-const CartTable = styled.table`
-  width: 100%;
-  border-collapse: collapse;
-`;
-
-const CartRow = styled.tr`
-  border-bottom: 1px solid #ccc;
-  &:last-child {
-    border-bottom: none;
-  }
-`;
-
-const CartCell = styled.td`
+const CartCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ccc;
+  margin-bottom: 10px;
   padding: 10px;
-  text-align: center;
-  @media (max-width: 768px) {
-    padding: 5px;
-    font-size: 0.8em;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
   }
 `;
 
-const TotalRow = styled.tr`
-  font-weight: bold;
+const CartInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+  }
 `;
 
 const QuantityControl = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 10px;
+
   & svg {
     margin: 0 10px;
     cursor: pointer;
+    width: 1.5em;
+    height: 1.5em;
   }
+
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+  }
+`;
+
+const RemoveIcon = styled(FiXCircle)`
+  cursor: pointer;
+  width: 2em;
+  height: 2em;
 `;
 
 const TableContainer = styled.div`
@@ -45,14 +57,12 @@ const TableContainer = styled.div`
 function CartPage() {
   const { cart, removeFromCart, updateQuantity } = useContext(CartContext);
 
-  const increaseQuantity = (product, variant, quantity) => {
-    updateQuantity(product.id, variant.size, quantity + 1);
+  const increaseQuantity = (id, size) => {
+    updateQuantity(id, size, 1);
   };
 
-  const decreaseQuantity = (product, variant, quantity) => {
-    if (quantity > 1) {
-      updateQuantity(product.id, variant.size, quantity - 1);
-    }
+  const decreaseQuantity = (id, size) => {
+    updateQuantity(id, size, -1);
   };
 
   const getTotalPrice = (price, quantity) => {
@@ -70,6 +80,7 @@ function CartPage() {
     }, 0).toFixed(2);
   };
 
+  // If cart is empty, return a message
   if (cart.length === 0) {
     return <h2>Your cart is empty.</h2>;
   }
@@ -78,42 +89,35 @@ function CartPage() {
     <div>
       <h1>Your Cart</h1>
       <TableContainer>
-        <CartTable>
-          <thead>
-            <tr>
-              <th>Item</th>
-              <th>Size</th>
-              <th>Quantity</th>
-              <th>Cost</th>
-              <th>Total</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((item, index) => (
-              <CartRow key={index}>
-                <CartCell>{item.product.name}</CartCell>
-                <CartCell>{item.variant.size}</CartCell>
-                <CartCell>
-                  <QuantityControl>
-                    <FiMinusCircle size={24} onClick={() => decreaseQuantity(item.product, item.variant, item.quantity)} />
-                    {item.quantity}
-                    <FiPlusCircle size={24} onClick={() => increaseQuantity(item.product, item.variant, item.quantity)} />
-                  </QuantityControl>
-                </CartCell>
-                <CartCell>${item.variant.price ? item.variant.price.toFixed(2) : "N/A"}</CartCell>
-                <CartCell>${getTotalPrice(item.variant.price, item.quantity)}</CartCell>
-                <CartCell>
-                  <FiXCircle size={24} onClick={() => removeFromCart(item.product.id, item.variant.size)} />
-                </CartCell>
-              </CartRow>
-            ))}
-            <TotalRow>
-              <CartCell colSpan="4">Total</CartCell>
-              <CartCell>${getCartTotal()}</CartCell>
-            </TotalRow>
-          </tbody>
-        </CartTable>
+        {cart.map((item, index) => (
+          <CartCard key={index}>
+            <CartInfo>
+              <div>
+                <strong>Product:</strong> {item.product.name}<br/>
+                <strong>Size:</strong> {item.variant.size}
+              </div>
+              <div>
+                <strong>Price:</strong> ${item.variant.price ? item.variant.price.toFixed(2) : "N/A"}
+              </div>
+            </CartInfo>
+            <CartInfo>
+              <QuantityControl>
+                <FiMinusCircle onClick={() => decreaseQuantity(item.product.id, item.variant.size)} />
+                {item.quantity}
+                <FiPlusCircle onClick={() => increaseQuantity(item.product.id, item.variant.size)} />
+              </QuantityControl>
+              <div>
+                <strong>Total:</strong> ${getTotalPrice(item.variant.price, item.quantity)}
+              </div>
+              <div>
+                <RemoveIcon onClick={() => removeFromCart(item.product.id, item.variant.size)} />
+              </div>
+            </CartInfo>
+          </CartCard>
+        ))}
+        <div>
+          <strong>Total:</strong> ${getCartTotal()}
+        </div>
       </TableContainer>
     </div>
   );
