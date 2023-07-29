5a50108c1d98b8e93cb30ad117aadaab9699aa41
Rob-Krisko <kriskora22@gmail.com>
2023-07-29 18:42:35 -0400

    Cart updates

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,6 +1,15 @@
 import React, { useContext, useState } from 'react';
 import { CartContext } from '../context/CartContext';
 import emailjs from 'emailjs-com';
+import styled from 'styled-components';
+
+const CartItem = styled.div`
+  border: 1px solid #ccc;
+  margin-bottom: 10px;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 function CheckoutPage() {
   const { cart } = useContext(CartContext);
@@ -10,17 +19,32 @@ function CheckoutPage() {
     setFromName(event.target.value);
   };
 
+  const getTotalPrice = (price, quantity) => {
+    if (price) {
+      return (price * quantity).toFixed(2);
+    } else {
+      return "N/A";
+    }
+  };
+
+  const getCartTotal = () => {
+    return cart.reduce((total, item) => {
+      const price = item?.variant?.price;
+      return price ? total + (price * item.quantity) : total;
+    }, 0).toFixed(2);
+  };
+
   const placeOrder = (event) => {
     event.preventDefault();
 
     // Generate cart content
     const message = cart.map(item => 
-      `Product: ${item.product.name} - Size: ${item.variant.size} - Quantity: ${item.quantity}`
+      `Product: ${item.product.name} - Size: ${item.variant.size} - Quantity: ${item.quantity} - Total: $${getTotalPrice(item.variant.price, item.quantity)}`
     ).join('\n');
 
     const templateParams = {
       from_name: fromName,
-      message: message
+      message: `${message}\n\nGrand Total: $${getCartTotal()}`
     };
 
     emailjs.send('service_2tkgwa8', 'template_8qucp12', templateParams, 'RvBl8MmqRGKx1RdcR')
@@ -39,6 +63,18 @@ function CheckoutPage() {
           Your Name:
           <input type="text" value={fromName} onChange={handleNameChange} required />
         </label>
+        
+        <h2>Review Your Order:</h2>
+        {cart.map((item, index) => (
+          <CartItem key={index}>
+            <p><strong>Product:</strong> {item.product.name}</p>
+            <p><strong>Size:</strong> {item.variant.size}</p>
+            <p><strong>Quantity:</strong> {item.quantity}</p>
+            <p><strong>Total:</strong> ${getTotalPrice(item.variant.price, item.quantity)}</p>
+          </CartItem>
+        ))}
+        <p><strong>Grand Total:</strong> ${getCartTotal()}</p>
+        
         <button type="submit">Place Order</button>
       </form>
     </div>
